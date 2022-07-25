6efe75cd69d69051fe9a9c3aad453c32bb799e04
dlcruz13777 <78450877+dlcruz13777@user.noreply.github.com>
2022-07-25 13:45:11 -0700

    Created .env file and refactored files

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
+require('dotenv').config()
 
 // middleware
 app.use(express.static('./public'))
@@ -20,7 +21,7 @@ const port = 5000
 
 const start = async () => {
     try {
-        await connectDB()
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, console.log(`Server is listening on port ${port}`))
     } catch (err) {
         console.log(err)
