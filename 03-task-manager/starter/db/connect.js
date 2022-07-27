61b85d1d0786072a34e46d242f8b9238f36ffc2f
dlcruz13777 <78450877+dlcruz13777@user.noreply.github.com>
2022-07-27 10:35:19 -0700

    Connected app to mongodb

--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 
 
 const connectDB = (url) => {
+    console.log('Connected')
     return mongoose.connect(url, {
         useNewUrlParser: true, 
         useCreateIndex: true,
