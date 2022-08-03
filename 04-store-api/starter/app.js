01ac948c31dbbc6ed935d04a4c608994677aeab4
dlcruz13777 <78450877+dlcruz13777@user.noreply.github.com>
2022-08-03 09:57:07 -0700

    Connected app with middleware and db

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,39 @@
-console.log('04 Store API')
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const errorMiddleware = require('./middleware/error-handler')
+const notFoundMiddleware = require('./middleware/not-found')
+// async errors
+require('express-async-errors')
+
+// db and router
+const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
+
+// middleware
+app.use(express.json())
+app.use('/api/v1/products', productsRouter)
+app.use(errorMiddleware)
+app.use(notFoundMiddleware)
+
+// routes
+app.get('/', (req, res) => {
+    res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
+})
+
+// products routes
+
+
+const port = process.env.PORT || 5000
+
+const start = async () => {
+    try {
+        // connectDB
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Listening on port: ${port}`))
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+start()
