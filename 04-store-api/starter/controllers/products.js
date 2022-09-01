e8c5dccd9b924979942f357ff916bc4519253187
dlcruz13777 <78450877+dlcruz13777@user.noreply.github.com>
2022-09-01 14:52:21 -0700

    Refactored and added numericFilters

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -2,8 +2,8 @@ const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
     const products = await Product
-        .find({})
-        .sort('name')
+        .find({ price: { $gt: 30 } })
+        .sort('price')
         .select('name price')
     res.status(200).json({ nbHits: products.length, products })
 }
@@ -15,9 +15,11 @@ const getAllProducts = async (req, res) => {
         name,
         sort,
         fields,
+        numericFilters,
     } = req.query
     const queryObject = {}
 
+    // Filters
     if (featured){
         queryObject.featured = featured === 'true' ? true: false
     }
@@ -27,26 +29,50 @@ const getAllProducts = async (req, res) => {
     if (name){
         queryObject.name = { $regex: name, $options: 'i' }
     }
-    
-    // console.log(queryObject)
-    let result = Product.find(queryObject)
+    if (numericFilters){
+        const options = ['price', 'rating']
+        const regEx = /\b(<|<=|=|>=|>)\b/g
+        const operatorMap = {
+            '>': '$gt',
+            '>=': '$gte',
+            '=': '$eq',
+            '<=': '$lte',
+            '<': '$lt',
+        }
+        let filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`)
+        filters = filters
+            .split(',')
+            .forEach((item) => {
+                const [field, operator, value] = item.split('-')
+                if (options.includes(field)){
+                    queryObject[field] = { [operator]: Number(value) }
+            }
+        })
+    }
+
+    let results = Product.find(queryObject)
+
+    // Sortation and limiting fields
     if (sort){
         const sortList = sort.split(',').join(' ')
-        result = result.sort(sortList)
+        results = results.sort(sortList)
     } else {
-        result = result.sort('createdAt')
+        results = results.sort('createdAt')
     }
     if (fields){
         const fieldsList = fields.split(',').join(' ')
-        result = result.select(fieldsList)
+        results = results.select(fieldsList)
     }
+
+    // Pages
     const page = Number(req.query.page) || 1
     const limit = Number(req.query.limit) || 10
     const skip = (page - 1) * limit
     
-    result = result.skip(skip).limit(limit)
+    // Results
+    results = results.skip(skip).limit(limit)
 
-    const products = await result
+    const products = await results
     res.status(200).json({ nbHits: products.length, products })
 }
 
