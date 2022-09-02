c5d75dfb9d81cc0d4472c23a22b376e4eea2806f
dlcruz13777 <78450877+dlcruz13777@user.noreply.github.com>
2022-09-02 14:05:08 -0700

    Specified error subclass used, added jwt signing

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,9 +1,5 @@
-const CustomAPIError = require('../errors/custom-error')
-// check username, password in post(login) request
-// if exist create new JWT
-// send back to front-end
-
-// setup authentication so only the request with JWT can access the dashboard
+const jwt = require('jsonwebtoken')
+const { BadRequestError } = require('../errors')
 
 const login = async(req, res) => {
     const { username, password } = req.body
@@ -14,15 +10,25 @@ const login = async(req, res) => {
         // Check in the controller
 
     if (!username || !password){
-        throw new CustomAPIError('Please provide email and password', 400)
+        throw new BadRequestError('Please provide email and password')
     }
     
-    res.status(200).json({user: username, pass: password})
+    // Just for demo, normally provided by db
+    const id = new Date().getDate()
+    
+    // Try to keep payload small, better experience for user
+    // In production, use long, complex, and unguessable string value
+    const token = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn: '30d'})
+
+    res.status(200).json({msg: 'user created', token})
 }
 
 const dashboard = async (req, res) => {
-    const luckyNumber = Math.random() * 100
-    res.status(200).json({ msg: `Hello, user`, secret:`Here is your authorized data, your lucky number is ${luckyNumber}` })
+    const luckyNumber = Math.floor(Math.random() * 100)
+    res.status(200).json({ 
+        msg: `Hello, ${req.user.username}`, 
+        secret:`Here is your authorized data, your lucky number is ${luckyNumber}` 
+    })
 }
 
 module.exports = { login, dashboard }
