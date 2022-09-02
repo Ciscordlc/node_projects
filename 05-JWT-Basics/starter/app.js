bd57770e623ea10dec6325ec7c63f22d1cef9db1
dlcruz13777 <78450877+dlcruz13777@user.noreply.github.com>
2022-09-02 09:38:45 -0700

    Added import and use of middlewre mainRouter

--- 05-JWT-Basics/starter/app.js
+++ 05-JWT-Basics/starter/app.js
@@ -6,23 +6,25 @@ const app = express();
 
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+const mainRouter = require('./routes/main')
 
 // middleware
 app.use(express.static('./public'));
 app.use(express.json());
+app.use('/api/v1', mainRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
     app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
+      console.log(`Server is listening on port ${port}`)
     );
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    console.log(err);
   }
 };
 
