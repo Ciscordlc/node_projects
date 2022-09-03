457fbadb239c617bb73c354d8604846c174620de
dlcruz13777 <78450877+dlcruz13777@user.noreply.github.com>
2022-09-03 12:56:57 -0700

    Refactored and removed unnecessary comments

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -4,20 +4,11 @@ const { BadRequestError } = require('../errors')
 const login = async(req, res) => {
     const { username, password } = req.body
 
-    // 3 options to validate:
-        // Mongoose validation
-        // Joi
-        // Check in the controller
-
     if (!username || !password){
         throw new BadRequestError('Please provide email and password')
     }
     
-    // Just for demo, normally provided by db
     const id = new Date().getDate()
-    
-    // Try to keep payload small, better experience for user
-    // In production, use long, complex, and unguessable string value
     const token = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn: '30d'})
 
     res.status(200).json({msg: 'user created', token})
