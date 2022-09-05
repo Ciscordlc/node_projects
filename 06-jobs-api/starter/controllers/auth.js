0a2c9af5a1f9d437434f5689680577f420628539
dlcruz13777 <78450877+dlcruz13777@user.noreply.github.com>
2022-09-05 12:58:57 -0700

    Added user registration and token signing

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -1,5 +1,16 @@
+const User = require('../models/User')
+const { StatusCodes } = require('http-status-codes')
+const jwt = require('jsonwebtoken')
+
 const register = async (req, res) => {
-    res.send('register user')
+    // Optional, but can send more meaningful error messages with mongoose validators
+    // const { name, email, password } = req.body
+    // if (!name || !email || !password){ throw new BadRequestError('Must provide name, email, and password') }\
+    const user = await User.create({ ...req.body }) // Gets mongoose to handle all of the validation
+
+    const token = jwt.sign({ userID: user._id, name: user.name }, process.env.JWT_SECRET, { expiresIn: '30d' })
+    
+    res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
 }
 
 const login = async (req, res) => {
