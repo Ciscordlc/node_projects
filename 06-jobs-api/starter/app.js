ccaaa8c254584a6ea15264788650cb98d812c27d
dlcruz13777 <78450877+dlcruz13777@user.noreply.github.com>
2022-09-05 11:33:53 -0700

    Added router and connected to db

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -5,19 +5,17 @@ const express = require('express');
 
 const app = express();
 
+// routers
+const authRouter = require('./routes/auth')
+const jobsRouter = require('./routes/jobs')
 
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
-// extra packages
-
-// routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
-
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', jobsRouter)
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
@@ -25,6 +23,7 @@ const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
