446b6f27cb7916477127fd0f15eed2a26be07ce8
dlcruz13777 <78450877+dlcruz13777@user.noreply.github.com>
2022-09-05 13:10:04 -0700

    Moved web token signing to schema file

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -1,15 +1,12 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
-const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
     // Optional, but can send more meaningful error messages with mongoose validators
     // const { name, email, password } = req.body
     // if (!name || !email || !password){ throw new BadRequestError('Must provide name, email, and password') }\
     const user = await User.create({ ...req.body }) // Gets mongoose to handle all of the validation
-
-    const token = jwt.sign({ userID: user._id, name: user.name }, process.env.JWT_SECRET, { expiresIn: '30d' })
-    
+    const token = user.createJWT()
     res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
 }
 
