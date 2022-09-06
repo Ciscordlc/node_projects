d5942db3734d6742952c1ece9b05da044561744d
dlcruz13777 <78450877+dlcruz13777@user.noreply.github.com>
2022-09-06 16:32:07 -0700

    Added properties and timestamps

--- 06-jobs-api/starter/models/Job.js
+++ 06-jobs-api/starter/models/Job.js
@@ -1,10 +1,28 @@
 const mongoose = require('mongoose')
 
 const JobSchema = new mongoose.Schema({
-    name: {
+    company: {
         type: String,
-        required: [true, 'Must provide job name'],
-    }
-})
+        required: [true, 'Must provide company name'],
+        maxlength: 50,
+    },
+    position: {
+        type: String,
+        required: [true, 'Must provide position'],
+        maxlength: 100,
+    },
+    status: {
+        type: String,
+        enum: ['interview', 'declined', 'pending'],
+        default: 'pending',
+    },
+    createdBy: {
+        // Ties job model with user model
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'Must provide user']
+    },
+
+}, { timestamps: true }) // timestamps shows createdAt and updatedAt properties
 
 module.exports = mongoose.model('Job', JobSchema)
