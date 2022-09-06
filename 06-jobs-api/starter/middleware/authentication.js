f8a2716e852e434069b824591076d39bd80c8df3
dlcruz13777 <78450877+dlcruz13777@user.noreply.github.com>
2022-09-06 16:33:28 -0700

    Renamed variable and fixed line 15

--- 06-jobs-api/starter/middleware/authentication.js
+++ 06-jobs-api/starter/middleware/authentication.js
@@ -1,21 +1,22 @@
+const User = require('../models/User')
 const jwt = require('jsonwebtoken')
 const { UnauthenticatedError } = require('../errors')
 
 const authenticationMiddleware = async (req, res, next) => {
     const authHeader = req.headers.authorization
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        throw new UnauthenticatedError('No token provided')
+        throw new UnauthenticatedError('Authentication invalid')
     }
-
+    
     const token = authHeader.split(' ')[1]
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        const { user, token } = decoded
-        req.user = user.authenticationMiddleware
+        const payload = jwt.verify(token, process.env.JWT_SECRET)
+        // Attach the user to the job route
+        req.user = { userID: payload.userID, name: payload.name }
         next()
     } catch (err) {
-        throw new UnauthenticatedError('Not authorized to access this route')
+        throw new UnauthenticatedError('Authentication invalid')
     }
 }
 
