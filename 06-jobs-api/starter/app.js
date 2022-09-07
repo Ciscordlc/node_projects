cdbc45f631895ba14ce4058c9c6e666c6fcad83d
dlcruz13777 <78450877+dlcruz13777@user.noreply.github.com>
2022-09-07 14:43:15 -0700

    Added security packages

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 const authenticateUser = require('./middleware/authentication')
 const connectDB = require('./db/connect')
 const express = require('express');
@@ -14,7 +21,17 @@ const jobsRouter = require('./routes/jobs')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1)
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+}))
 app.use(express.json());
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 app.use(notFoundMiddleware);
